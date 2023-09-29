bce8ac1abdc3c160c43efa935b67a65d8b68bb88
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-09-29 02:14:26 +0000

    Add Validator.
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- api/v1/markdownview_webhook.go
+++ api/v1/markdownview_webhook.go
@@ -17,7 +17,10 @@ limitations under the License.
 package v1
 
 import (
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -57,15 +60,54 @@ var _ webhook.Validator = &MarkdownView{}
 func (r *MarkdownView) ValidateCreate() (admission.Warnings, error) {
 	markdownviewlog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil, nil
+	return r.validate()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *MarkdownView) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	markdownviewlog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
+	return r.validate()
+}
+func (r *MarkdownView) validate() (admission.Warnings, error) {
+	var errs field.ErrorList
+
+	if r.Spec.Replicas < 1 || r.Spec.Replicas > 5 {
+		errs = append(errs,
+			field.Invalid(
+				field.NewPath("spec", "replicas"),
+				r.Spec.Replicas,
+				"replicas must be in the range of 1 to 5.",
+			),
+		)
+	}
+
+	hasSummary := false
+	for name := range r.Spec.Markdowns {
+		if name == "SUMMARY.md" {
+			hasSummary = true
+		}
+	}
+
+	if !hasSummary {
+		errs = append(errs,
+			field.Required(
+				field.NewPath("spec", "markdowns"),
+				"markdowns must have SUMMARY.md.",
+			),
+		)
+	}
+
+	if len(errs) > 0 {
+		err := apierrors.NewInvalid(
+			schema.GroupKind{Group: GroupVersion.Group, Kind: "MarkdownView"},
+			r.Name,
+			errs,
+		)
+		markdownviewlog.Error(err, "validation error", "name", r.Name)
+		return nil, err
+	}
+
 	return nil, nil
 }
 
